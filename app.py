36a80ae77c77606b5531ae8690a35d8480648bbe
Alejandro <alejandrosantandertoto@gmail.com>
2024-11-13 10:17:38 -0500

    MOD: se agregan nuevas cosas a la interfaz inicial

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def iniciarPrograma():
     if 'controlador' not in st.session_state:
         st.session_state.controlador = Controller()
 
-    st.set_page_config(page_title="Inicio", page_icon="🛎😎", layout="wide", initial_sidebar_state="collapsed")
+    st.set_page_config(page_title="Inicio", page_icon="😎", layout="wide", initial_sidebar_state="collapsed")
 
     html_code = """
     <!DOCTYPE html> 
@@ -36,7 +36,15 @@ def iniciarPrograma():
                 .banner img { 
                     max-width: 55%; 
                     height: auto; 
-                } .content { 
+                } .container {
+                     position: relative; 
+                     max-width: 100%; 
+                     overflow: hidden;
+                }.zoom {
+                    transition: transform 0.5s ease;
+                } .container:hover .zoom {
+                    transform: scale(1.1);
+                }.content { 
                     display: flex; 
                     justify-content: space-around; 
                     padding: 20px; 
@@ -60,7 +68,7 @@ def iniciarPrograma():
                     background-color: #FFFFFF;
                     border: none;
                     color: #494BA8;
-                    padding: 15px 32px;
+                    padding: 10px 20px;
                     text-align: center;
                     text-decoration: none;
                     display: inline-block;
@@ -77,7 +85,7 @@ def iniciarPrograma():
             <h1>Consultas de interés</h1> 
         </div> 
         <div class="banner"> 
-            <img src="https://thumbs.dreamstime.com/z/estudiantes-con-docente-estudiando-en-aula-de-diferentes-edades-sentados-juntos-mesas-estudio-grupal-profesor-270894481.jpg" alt="Estudiantes en un aula"><p> 
+            <img src="https://thumbs.dreamstime.com/z/estudiantes-con-docente-estudiando-en-aula-de-diferentes-edades-sentados-juntos-mesas-estudio-grupal-profesor-270894481.jpg" alt="Estudiantes en un aula" class="zoom"><p> 
             <div class="banner"> 
                 <div>
                     <p style="font-size: 25px;">Conozca las instituciones y programas académicos de educación superior autorizados por el Ministerio de Educación Nacional</p>
@@ -95,11 +103,13 @@ def iniciarPrograma():
                  Instituciones Técnicas Profesionales, Instituciones Tecnológicas y Universidades.<br>
                 Cada una ofrece programas de pregrado y posgrado en diversas áreas.</p> 
                 <p style="font-size: 20px;">Este sistema provee información detallada sobre las instituciones y programas de educación superior en Colombia.</p> 
-                <img src="https://static.misionesonline.news/wp-content/uploads/2020/11/Clases-universitarias-AP-def.jpg" </img>
+                <div class="container">
+                    <img src="https://static.misionesonline.news/wp-content/uploads/2020/11/Clases-universitarias-AP-def.jpg" class="zoom"</img>
+                </div>
             </div> 
             <div class="right"> 
-                <button>Instituciones</button> 
-                <button>Obtener Información Personalizada</button>
+                <button>Instituciones</button>
+                <button>Información personalizada</button>
                 <button>Información Estadística</button> 
                 <button>Estadísticas Destacadas</button> 
             </div>
@@ -110,7 +120,44 @@ def iniciarPrograma():
     # Incrustar HTML solo si estamos en la página principal
     if "section" not in st.query_params:
         st.markdown(html_code, unsafe_allow_html=True)
-    st.write('hola')
+    st.write('')
+    col1, col2, col3 = st.columns(3)
+    with col1 :
+        st.title('SNIES')
+        st.markdown("""<div style="background-color:#494BA8; padding: 20px; border-radius: 5px;">
+                            <p style="font-size: 20px;">El Sistema Nacional de Información de la Educación Superior (SNIES) en Colombia recopila datos detallados sobre las 
+                            instituciones y programas académicos de educación superior. Este análisis de datos, que abarca aspectos como inscripción, 
+                            admisión, matrícula y graduación, es fundamental para tomar decisiones informadas en el ámbito educativo.
+                            </p>
+                    </div>
+                    <div>
+                        <img src="https://www.uniautonoma.edu.co/sites/default/files/evento/g.jpg">
+                    </div>""", unsafe_allow_html=True)
+    with col2:
+        st.subheader('Importancia del Análisis de Datos en el SNIES')
+        st.markdown(""" <div style="background-color:#494BA8; padding: 20px; border-radius: 5px;">
+                            <h3>Mejora del Diseño Curricular</h3><p style="font-size: 20px;">El análisis de las tendencias de 
+                                matrícula y graduación puede identificar áreas que necesitan ajustes curriculares, asegurando 
+                                que los programas sean relevantes y atractivos para los estudiantes.</p>
+                            <h3>Decisiones Estratégicas</h3><p style="font-size: 20px;">Datos históricos y tendencias ayudan a las 
+                            instituciones a decidir sobre la apertura de nuevos programas, modificaciones de los existentes, y la 
+                            asignación de recursos. Esto permite a las instituciones responder de manera proactiva a las demandas 
+                            del mercado educativo y laboral.</p>
+                    </div>""", unsafe_allow_html=True)
+    with col3:
+
+        tab1, tab2, tab3 = st.tabs(["INFORMACION", "NOTICIAS", "ESTADÍSTICAS"])
+        with tab1:
+           st.header("MIRA LA INFORMACIÓN RESPECTO A LOS DATOS")
+           st.write("Aquí va el contenido de la Pestaña 1")
+        with tab2:
+           st.header("")
+           st.write("Aquí va el contenido de la Pestaña 2")
+        with tab3:
+           st.header("ESTADÍSTICAS RELEVANTES")
+           st.write("Aquí va el contenido de la Pestaña 3")
+
+
 if __name__ == "__main__":
     iniciarPrograma()
 
--- pages/01Quienes_somos.py
+++ pages/01Quienes_somos.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
 def quienesSomos(controlador):
+    st.set_page_config(page_title="Quienes Somos", layout="wide",page_icon="🤫", initial_sidebar_state="collapsed")
     st.title('¿QUIENES SOMOS?')
+    st.markdown(""" 
+        <p style="font-size: 25px;">Somos un grupo de desarrolladores que ofrecemos una applicación web
+            que permite el análisis de datos del SNIES</p>""", unsafe_allow_html=True)
 
 quienesSomos(st.session_state.controlador)
