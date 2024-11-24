9e6ec5e291fa6955d752649545047c61df211f81
Jorge Osorio <jorluos@hotmail.com>
2024-11-23 20:30:12 -0500

    MOD CLASE ESCRITURA

--- Model/src/gestor_escritura.py
+++ Model/src/gestor_escritura.py
@@ -2,23 +2,20 @@ class gestor_escritura:
     def __init__(self, archivo):
         self.archivo = archivo
 
-    def guardar_excel(self, nombre_archivo, encabezados):
+    def guardar_excel(self, nombre_archivo):
         try:
-            self.archivo.columns = encabezados
             self.archivo.to_excel(nombre_archivo, index=False, engine="openpyxl")
         except Exception as e:
             raise ValueError(f"Error al guardar el archivo Excel: {e}")
     
-    def guardar_json(self, nombre_archivo, encabezados):
+    def guardar_json(self, nombre_archivo):
         try:
-            self.archivo.columns = encabezados
-            self.archivo.to_json(nombre_archivo, index=False, engine="openpyxl")
+            self.archivo.to_json(nombre_archivo, index=False)
         except Exception as e:
             raise ValueError(f"Error al guardar el archivo JSON: {e}")
 
-    def guardar_csv(self, nombre_archivo, encabezados):
+    def guardar_csv(self, nombre_archivo):
         try:
-            self.archivo.columns = encabezados
             self.archivo.to_csv(nombre_archivo, index=False)
         except Exception as e:
             raise ValueError(f"Error al guardar el archivo CSV: {e}")
